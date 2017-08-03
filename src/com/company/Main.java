a0e2d74432d40f254226ec3a308d8dc738efd5a4
PDG <zja1vb2xj3@naver.com>
2017-08-04 03:16:45 +0900

    유닛 타입 추가 true 시 생물 scv : 기계 수리가능 메딕 : 생물 치료가능

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,6 +12,8 @@ public class Main {
         Medic medic = new Medic();
 
         int recoveredHP = medic.recoveryHP(marine.hp, marine.getMaxHP(), marine.name, marine.unitType);
+
+        if(recoveredHP != -1)
         marine.increaseHP(marine.name, recoveredHP-marine.hp);
 
         Tank tank = new Tank();
@@ -20,10 +22,18 @@ public class Main {
         damage += hydra.attackUnit(tank.name);
 
         tank.decreaseHP(tank.name, damage);
-        //메딕은 사람만 치료가능 //SCV는 기계만 치료가능
+
         Scv scv = new Scv();
+
         recoveredHP = scv.repairHP(tank.hp, tank.getMaxHP(), tank.name, tank.unitType);
+
+        if(recoveredHP != -1)
         tank.increaseHP(tank.name, recoveredHP-tank.hp);
+
+        //scv는 기계 타입만 수리가능
+        scv.repairHP(marine.hp, marine.getMaxHP(), marine.name, marine.unitType);
+        //medic은 생물만 치료가능
+        medic.recoveryHP(tank.hp, tank.getMaxHP(), tank.name, tank.unitType);
     }
 
 
